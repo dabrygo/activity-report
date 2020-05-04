da8eb7561b130fa499accc49ac8e44c9b9271c68
Daniel <dbgodfrey89@msn.com>
2020-05-03 19:22:24 -0600

    Outline a solution

---
+++ activity_report.py
@@ -0,0 +1,9 @@
+
+# Ask for total time
+# Ask for contract ID
+# Ask for contract name
+# Ask for contract time percentage/allocation
+# Ask for time spent
+# Ask for goal
+# Ask for action
+# Save to file when done
