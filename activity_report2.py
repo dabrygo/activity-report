62d9b85d3c4d881e855e2862cbeb1aeba9dbbf83
Daniel <dbgodfrey89@msn.com>
2020-06-01 04:48:04 -0600

    Start another simpler approach

---
+++ activity_report2.py
@@ -0,0 +1,6 @@
+contract = input("Contract #: ")
+time_spent = input("Time spent (hr): ")
+goal = input("What goal?")
+action = input("What action?")
+
+print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
