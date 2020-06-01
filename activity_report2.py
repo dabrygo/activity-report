d9e8d5bfec9d8d3aae42cc1a43ecc98cba3a1f92
Daniel <dbgodfrey89@msn.com>
2020-06-01 04:52:33 -0600

    Save inputs to file

--- activity_report2.py
+++ activity_report2.py
@@ -1,6 +1,13 @@
+import csv
+
 contract = input("Contract #: ")
 time_spent = input("Time spent (hr): ")
-goal = input("What goal?")
-action = input("What action?")
+goal = input("What goal? ")
+action = input("What action? ")
 
 print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
+
+with open("activity_report.dat", 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['contract', 'time_spent', 'goal', 'action'])
+    writer.writerow([contract, time_spent, goal, action])
