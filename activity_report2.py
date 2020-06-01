1d2fb7513470183e9e5e7608a9df6e865ff1822a
Daniel <dbgodfrey89@msn.com>
2020-06-01 07:56:51 -0600

    Load from file and inform user of existing goals

--- activity_report2.py
+++ activity_report2.py
@@ -1,17 +1,38 @@
 import csv
 import datetime
+import os.path
+
+DATA_FILE = "activity_report.dat"
 
 date = datetime.datetime.now()
 contract = input("Contract #: ")
 time_spent = input("Time spent (hr): ")
-goal = input("What goal? ")
-action = input("What action? ")
 
+def load_data(file_=DATA_FILE):
+    if os.path.exists(file_):
+        with open(file_) as f:
+            reader = csv.DictReader(f)
+            return [row for row in reader]
+    return []
+
+records = load_data()
+contracts = set(r['contract'] for r in records)
+print(contracts)
+if contract in contracts:
+    goals = set(r['goal'] for r in records if r['contract'] == contract)
+    print("Contract currently has these goals:")
+    for goal in goals:
+        print(f"  * {goal}")
+goal = input("What goal? ")
+
+action = input("What action? ")
 print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
 
-with open("activity_report.dat", 'w') as f:
+file_exists = os.path.exists(DATA_FILE)
+with open(DATA_FILE, 'a', newline='') as f:
     writer = csv.writer(f)
-    header = ['date', 'contract', 'time_spent', 'goal', 'action']
-    writer.writerow(header)
+    if not file_exists:
+        header = ['date', 'contract', 'time_spent', 'goal', 'action']
+        writer.writerow(header)
     date_id = date.strftime("%Y%m%d")
     writer.writerow([date_id, contract, time_spent, goal, action])
