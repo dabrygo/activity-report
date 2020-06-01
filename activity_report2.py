79a3b5b6516e41ce599c4710e35a6542c210e48f
Daniel <dbgodfrey89@msn.com>
2020-06-01 05:00:30 -0600

    Stamp entry with today's date

--- activity_report2.py
+++ activity_report2.py
@@ -1,5 +1,7 @@
 import csv
+import datetime
 
+date = datetime.datetime.now()
 contract = input("Contract #: ")
 time_spent = input("Time spent (hr): ")
 goal = input("What goal? ")
@@ -9,5 +11,7 @@ print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to 
 
 with open("activity_report.dat", 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(['contract', 'time_spent', 'goal', 'action'])
-    writer.writerow([contract, time_spent, goal, action])
+    header = ['date', 'contract', 'time_spent', 'goal', 'action']
+    writer.writerow(header)
+    date_id = date.strftime("%Y%m%d")
+    writer.writerow([date_id, contract, time_spent, goal, action])
