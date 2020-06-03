03b951ca9b2b55bfbacb58c4d8c71bfdbf42ff0d
Daniel <dbgodfrey89@msn.com>
2020-06-02 21:20:07 -0600

    Add a loop around option selection

--- activity_report.py
+++ activity_report.py
@@ -11,38 +11,46 @@ def load_data(file_=DATA_FILE):
             return [row for row in reader]
     return []
 
-option = int(input("What do you want to do?\n  1. Add entry\n  2. Make report\n>>> "))
-if option == 1:
-    date = datetime.datetime.now()
-    contract = input("Contract #: ")
-    time_spent = input("Time spent (hr): ")
 
-    records = load_data()
-    contracts = set(r['contract'] for r in records)
-    print(contracts)
-    if contract in contracts:
-        goals = set(r['goal'] for r in records if r['contract'] == contract)
-        print("Contract currently has these goals:")
-        for goal in goals:
-            print(f"  * {goal}")
-    goal = input("What goal? ")
+prompt = '''\
+What do you want to do?
+  1. Add entry
+  2. Make report
+  3. Exit
+>>> \
+'''
+while (option := int(input(prompt))) != 3:
+    if option == 1:
+        date = datetime.datetime.now()
+        contract = input("Contract #: ")
+        time_spent = input("Time spent (hr): ")
 
-    action = input("What action? ")
-    print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
+        records = load_data()
+        contracts = set(r['contract'] for r in records)
+        print(contracts)
+        if contract in contracts:
+            goals = set(r['goal'] for r in records if r['contract'] == contract)
+            print("Contract currently has these goals:")
+            for goal in goals:
+                print(f"  * {goal}")
+        goal = input("What goal? ")
 
-    file_exists = os.path.exists(DATA_FILE)
-    if not file_exists:
-        record_id = 0
-    else:
-        record_id = max(int(r['id']) for r in records) + 1
-    with open(DATA_FILE, 'a', newline='') as f:
-        writer = csv.writer(f)
+        action = input("What action? ")
+        print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
+
+        file_exists = os.path.exists(DATA_FILE)
         if not file_exists:
-            header = ['id', 'date', 'contract', 'time_spent', 'goal', 'action']
-            writer.writerow(header)
-        date_id = date.strftime("%Y%m%d")
-        writer.writerow([record_id, date_id, contract, time_spent, goal, action])
-elif option == 2:
-    pass
-else:
-    print(f"I don't recognize option '{option}'")
+            record_id = 0
+        else:
+            record_id = max(int(r['id']) for r in records) + 1
+        with open(DATA_FILE, 'a', newline='') as f:
+            writer = csv.writer(f)
+            if not file_exists:
+                header = ['id', 'date', 'contract', 'time_spent', 'goal', 'action']
+                writer.writerow(header)
+            date_id = date.strftime("%Y%m%d")
+            writer.writerow([record_id, date_id, contract, time_spent, goal, action])
+    elif option == 2:
+        pass
+    else:
+        print(f"I don't recognize option '{option}'")
