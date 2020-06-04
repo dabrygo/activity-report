8d182d5eb660c0fc512f1964671fed97962130d9
Daniel <dbgodfrey89@msn.com>
2020-06-03 20:53:19 -0600

    Generate a basic markdown report

--- activity_report.py
+++ activity_report.py
@@ -1,3 +1,4 @@
+from collections import defaultdict as ddict
 import csv
 import datetime
 import os.path
@@ -38,6 +39,7 @@ while (option := int(input(prompt))) != 3:
         action = input("What action? ")
         print(f"You spent {time_spent} hrs on '{contract}' doing '{action}' in order to achieve '{goal}'")
 
+        # Save data
         file_exists = os.path.exists(DATA_FILE)
         if not file_exists:
             record_id = 0
@@ -51,6 +53,34 @@ while (option := int(input(prompt))) != 3:
             date_id = date.strftime("%Y%m%d")
             writer.writerow([record_id, date_id, contract, time_spent, goal, action])
     elif option == 2:
-        pass
+        records = load_data()
+        organized = ddict(list)
+        for record in records:
+            contract = record['contract']
+            goal = record['goal']
+            key = (contract, goal)
+
+            date = record['date']
+            time_spent = record['time_spent']
+            action = record['action']
+            value = (date, time_spent, action)
+            organized[key].append(value)
+        print(f'# Activity Report')
+        previous_contract = None
+        previous_goal = None
+        for key, values in organized.items():
+            contract, goal = key
+
+            if contract != previous_contract:
+                print(f'## {contract}')
+            previous_contract = contract
+
+            if goal != previous_goal:
+                print(f"  * {goal}")
+            previous_goal = goal
+
+            for value in values:
+                date, time_spent, action = value
+                print(f"    - {action} ({time_spent} hrs)")
     else:
         print(f"I don't recognize option '{option}'")
